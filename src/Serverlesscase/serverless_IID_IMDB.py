39712172f48a0c25f1f767fd4613146197d916fb
Sree bhargavi balija <115974245+Sreebhargavibalijaa@users.noreply.github.com>
2024-01-22 17:37:28 -0800

    Add files via upload

---
+++ src/Serverlesscase/serverless_IID_IMDB.py
@@ -0,0 +1,303 @@
+print("y34423es")
+
+import time
+from collections import OrderedDict
+import os
+import random
+import warnings
+
+import flwr as fl
+import torch
+import psutil
+from torch.utils.data import DataLoader
+from datasets import load_dataset
+from evaluate import load as load_metric
+
+from transformers import AutoTokenizer, DataCollatorWithPadding
+from transformers import AutoModelForSequenceClassification
+from transformers import AdamW
+from transformers import logging
+
+"""Next we will set some global variables and disable some of the logging to clear out our output."""
+
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+logging.set_verbosity(logging.ERROR)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+warnings.simplefilter('ignore')
+
+DEVICE = torch.device("cpu")
+print("yes")
+CHECKPOINT = "albert-base-v2"  # transformer model checkpoint
+NUM_CLIENTS = 10
+NUM_ROUNDS = 20
+
+"""## Standard Hugging Face workflow
+
+### Handling the data
+
+To fetch the IMDB dataset, we will use Hugging Face's `datasets` library. We then need to tokenize the data and create `PyTorch` dataloaders, this is all done in the `load_data` function:
+"""
+before_communication_cpu_percent = psutil.cpu_percent()
+current_process = psutil.Process()
+
+memory_info_after = current_process.memory_info()
+start = time.time()
+def load_data():
+    """Load IMDB data (training and eval)"""
+    raw_datasets = load_dataset("imdb")
+    raw_datasets = raw_datasets.shuffle(seed=42)
+
+    # remove unnecessary data split
+    del raw_datasets["unsupervised"]
+
+    tokenizer = AutoTokenizer.from_pretrained(CHECKPOINT)
+
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], truncation=True)
+
+    # Select 20 random samples to reduce the computation cost
+    train_population = random.sample(range(len(raw_datasets["train"])), 100)
+    test_population = random.sample(range(len(raw_datasets["test"])), 100)
+
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+    tokenized_datasets["train"] = tokenized_datasets["train"].select(train_population)
+    tokenized_datasets["test"] = tokenized_datasets["test"].select(test_population)
+
+    tokenized_datasets = tokenized_datasets.remove_columns("text")
+    tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    trainloader = DataLoader(
+        tokenized_datasets["train"],
+        shuffle=True,
+        batch_size=32,
+        collate_fn=data_collator,
+    )
+
+    testloader = DataLoader(
+        tokenized_datasets["test"], batch_size=32, collate_fn=data_collator
+    )
+
+    return trainloader, testloader,train_population,test_population
+
+"""### Training and testing the model
+
+Once we have a way of creating our trainloader and testloader, we can take care of the training and testing. This is very similar to any `PyTorch` training or testing loop:
+"""
+
+def train(net, trainloader, epochs):
+    optimizer = AdamW(net.parameters(), lr=5e-5)
+    net.train()
+    for _ in range(epochs):
+        for batch in trainloader:
+            batch = {k: v.to(DEVICE) for k, v in batch.items()}
+            outputs = net(**batch)
+            loss = outputs.loss
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+
+def test(net, testloader):
+    metric = load_metric("accuracy")
+    loss = 0
+    net.eval()
+    for batch in testloader:
+        batch = {k: v.to(DEVICE) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = net(**batch)
+        logits = outputs.logits
+        loss += outputs.loss.item()
+        predictions = torch.argmax(logits, dim=-1)
+        metric.add_batch(predictions=predictions, references=batch["labels"])
+    loss /= len(testloader.dataset)
+    accuracy = metric.compute()["accuracy"]
+    return loss, accuracy
+
+"""### Creating the model itself
+
+To create the model itself, we will just load the pre-trained alBERT model using Hugging Face’s `AutoModelForSequenceClassification` :
+"""
+
+net = AutoModelForSequenceClassification.from_pretrained(
+    CHECKPOINT, num_labels=2
+).to(DEVICE)
+
+"""## Federating the example
+
+The idea behind Federated Learning is to train a model between multiple clients and a server without having to share any data. This is done by letting each client train the model locally on its data and send its parameters back to the server, which then aggregates all the clients’ parameters together using a predefined strategy. This process is made very simple by using the [Flower](https://github.com/adap/flower) framework. If you want a more complete overview, be sure to check out this guide: [What is Federated Learning?](https://flower.dev/docs/tutorial/Flower-0-What-is-FL.html)
+
+### Creating the IMDBClient
+
+To federate our example to multiple clients, we first need to write our Flower client class (inheriting from `flwr.client.NumPyClient`). This is very easy, as our model is a standard `PyTorch` model:
+"""
+
+class IMDBClient(fl.client.NumPyClient):
+    def __init__(self, net, trainloader, testloader):
+        self.net = net
+        self.trainloader = trainloader
+        self.testloader = testloader
+
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for _, val in self.net.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        params_dict = zip(self.net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+        self.net.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        print("Training Started...")
+        train(self.net, self.trainloader, epochs=1)
+        print("Training Finished.")
+        return self.get_parameters(config={}), len(self.trainloader), {}
+    def train_model(self):
+        """Train the model."""
+        optimizer = AdamW(self.net.parameters(), lr=5e-5)
+        self.net.train()
+        for _ in range(1):  # Assuming 1 epoch for simplicity
+            for batch in self.trainloader:
+                batch = {k: v.to(DEVICE) for k, v in batch.items()}
+                outputs = self.net(**batch)
+                loss = outputs.loss
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, accuracy = test(self.net, self.testloader)
+        return float(loss), len(self.testloader), {"accuracy": float(accuracy), "loss": float(loss)}
+    def evaluate_model(self):
+        """Evaluate the model."""
+        metric = load_metric("accuracy")
+        total_loss = 0
+        self.net.eval()
+        for batch in self.testloader:
+            batch = {k: v.to(DEVICE) for k, v in batch.items()}
+            with torch.no_grad():
+                outputs = self.net(**batch)
+            logits = outputs.logits
+            total_loss += outputs.loss.item()
+            predictions = torch.argmax(logits, dim=-1)
+            metric.add_batch(predictions=predictions, references=batch["labels"])
+        total_loss /= len(self.testloader.dataset)
+        accuracy = metric.compute()["accuracy"]
+        return total_loss, accuracy
+
+"""The `get_parameters` function lets the server get the client's parameters. Inversely, the `set_parameters` function allows the server to send its parameters to the client. Finally, the `fit` function trains the model locally for the client, and the `evaluate` function tests the model locally and returns the relevant metrics.
+
+### Generating the clients
+
+In order to simulate the federated setting we need to provide a way to instantiate clients for our simulation. Here, it is very simple as every client will hold the same piece of data (this is not realistic, it is just used here for simplicity sakes).
+"""
+
+trainloader, testloader,train_population,test_population = load_data()
+def client_fn(cid):
+  return IMDBClient(net, trainloader, testloader)
+
+"""## Starting the simulation
+
+We now have all the elements to start our simulation. The `weighted_average` function is there to provide a way to aggregate the metrics distributed amongst the clients (basically to display a nice average accuracy at the end of the training). We then define our strategy (here `FedAvg`, which will aggregate the clients weights by doing an average).
+
+Finally, `start_simulation` is used to start the training.
+"""
+
+def weighted_average1(metrics):
+  accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+  losses = [num_examples * m["loss"] for num_examples, m in metrics]
+  examples = [num_examples for num_examples, _ in metrics]
+  return {"accuracy": sum(accuracies) / sum(examples), "loss": sum(losses) / sum(examples)}
+
+strategy = fl.server.strategy.FedAvg(
+    fraction_fit=1.0,
+    fraction_evaluate=1.0,
+    evaluate_metrics_aggregation_fn=weighted_average1,
+)
+
+# fl.simulation.start_simulation(
+#     client_fn=client_fn,
+#     num_clients=NUM_CLIENTS,
+#     config=fl.server.ServerConfig(num_rounds=NUM_ROUNDS),
+#     strategy=strategy,
+#     client_resources={"num_cpus": 1, "num_gpus": 0},
+#     ray_init_args={"log_to_driver": False, "num_cpus": 1, "num_gpus": 0}
+# )
+
+"""Note that this is a very basic example, and a lot can be added or modified, it was just to showcase how simply we could federate a Hugging Face workflow using Flower. The number of clients and the data samples are intentionally very small in order to quickly run inside Colab, but keep in mind that everything can be tweaked and extended."""
+# ... (previous code)
+
+# Define a global model
+global_model = AutoModelForSequenceClassification.from_pretrained(
+    CHECKPOINT, num_labels=2
+).to(DEVICE)
+def evaluate_global_model(global_model, testloader):
+    global_model.eval()
+    loss, accuracy = test(global_model, testloader)
+    return accuracy
+
+import copy
+
+global_model = copy.deepcopy(net)  # Create a copy of the initial model
+def evaluate_global_model(model, testloader):
+    """Evaluate the global model on the test dataset."""
+    loss, accuracy = test(model, testloader)
+    return accuracy
+
+# Define the global model for evaluation
+global_model = AutoModelForSequenceClassification.from_pretrained(CHECKPOINT, num_labels=2).to(DEVICE)
+strategy=strategy,
+trained_data = []
+tested_data = []
+global_model_Accuracies = []
+
+for round_num in range(NUM_ROUNDS):
+    aggregated_params = []
+    for _ in range(NUM_CLIENTS):
+        trainloader, testloader,train_population,test_population = load_data()
+        trained_data.append(train_population)
+        tested_data.append(test_population)
+        client = IMDBClient(global_model, trainloader, testloader)
+        client.train_model()
+        client_params = client.get_parameters(config={})
+        loss, accuracy = client.evaluate_model()
+        print("local_accuracy"+" :" + str(accuracy))
+        aggregated_params.append(client_params)
+
+    # Averaging the parameters
+    avg_params = [sum(param) / len(param) for param in zip(*aggregated_params)]
+    global_model.load_state_dict(OrderedDict({k: torch.Tensor(v) for k, v in zip(global_model.state_dict().keys(), avg_params)}))
+
+    # Evaluate the global model
+    print(global_model)
+
+
+    trainloader, testloader,train_population,test_population = load_data()   
+    global_accuracy = evaluate_global_model(global_model, testloader)
+    global_model_Accuracies.append(global_accuracy)
+    print(f"Global Model Accuracy: {global_accuracy * 100:.2f}%")
+    global_model.save_pretrained('my_model.h5')
+    end = time.time()
+# Get the file size in GB
+    file_size = os.path.getsize('my_model.h5') / (1024 * 1024 * 1024)
+    print(f"Model Size: {file_size} GB")
+    print(f"Latency: {(end-start)/60} min")
+
+after_communication_cpu_percent = psutil.cpu_percent()
+current_process = psutil.Process()
+
+memory_info_before = current_process.memory_info()
+
+# Calculate the communication overhead
+cpu_overhead = after_communication_cpu_percent - before_communication_cpu_percent
+memory_overhead =(memory_info_after.rss - memory_info_before.rss) / (1024 ** 3)  # Convert bytes to GB
+
+print(f"CPU Overhead: {cpu_overhead}%")
+print(f"Memory Usage: {memory_overhead:.2f} GB")
+print("trained_data")
+print(trained_data)
+print("tested_data")
+print(tested_data)
+print("global_model_Accuracies")
+print(global_model_Accuracies)
